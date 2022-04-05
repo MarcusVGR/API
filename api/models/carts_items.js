fc667ca7f329e780d0599908e3c880a7440ff47c
MarcusVGR <marcusvgr@gmail.com>
2022-04-05 18:35:58 -0300

    .\api

---
+++ api/models/carts_items.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Carts_items extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Carts_items.init({
+    cart_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER,
+    qty: DataTypes.INTEGER,
+    price: DataTypes.FLOAT,
+    total: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Carts_items',
+  });
+  return Carts_items;
+};
---
+++ api/models/products.js
@@ -0,0 +1,20 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    
+    static associate(models) {
+      // define association here
+    }
+  }
+  Products.init({
+    name: DataTypes.STRING,
+    price: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Products;
+};
