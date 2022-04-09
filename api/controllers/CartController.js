8e30cfa690ede84f91332004a9bbda1f6788c1cd
MarcusVGR <marcusvgr@gmail.com>
2022-04-09 20:54:19 -0300

    master

--- api/controllers/CartController.js
+++ api/controllers/CartController.js
@@ -3,7 +3,20 @@ const database = require('../models')
 class CartController {
     static async createCart(req, res){ //post carts
         const newCart = req.body
+
+        // {
+        //     id: Math.random() * 100;
+        //     nome:
+        //     qty: 
+        // }
         try {
+            // newCart.id = Math.random() * 100;
+
+            // const checkCart = await database.Carts.findOne({id: newCart.id});
+            // if(checkCart) {?
+                // gerar um novo id
+            // }
+
             const newCartCreated = await database.Carts.create(newCart)
             return res.status(200).json(newCartCreated)
 
@@ -24,14 +37,45 @@ class CartController {
     }
 
     static async addItem (req, res) { // put carts/items
-    // Eu preciso alterar na tabela de products e carts_items?
-        const { productId } = req.params
-        const { newQty } = req.body
+
+        const { id, productId } = req.params // supondo que é o id do cart
+        const { newQty } = req.body // supondo ser o produto
+        // {
+        //     newQty: 1
+        // }
+
         try {
+            // buscar o cart pelo id e ver se existe
+            // se não existir, você tem a opção de criar um ou retornar erro que não existe
+
+            // com o cart válido, busca o produto pelo productId e verifica se é válido
+            // se não existir, retornar erro que não existe
+
+
+            // nesse ponto voce tem o cart e o produto com um objeto.
+            // Cart 
+            // Product
+
+            // cria um novo cart item
+            // let cartItem = {
+            //     cart_id: // voce tem essa info
+            //     product_id: // voce tem essa info
+            //     qty: // voce tem 
+            //     price: // preço do produto (product.price)
+            //     total: // qty * product.price
+            // }
+            // const newCartItemCreated = await database.Carts_Items.create(cartItem)
+
+            // cart.total += cartItem.total
+            // basta salvar as atualizações do cart
+
+
             await database.Carts_items.update(newQty, {
-                where: { id: Number(productId) }})
+                where: { id: Number(id) }})
+
+
             const productQty = await database.Carts_Items.findOne({
-                where: { id: Number(productId) }})
+                where: { id: Number(id) }})
             return res.status(200).json(productQty)
         } catch (error) {
             return res.status(500).json(error.message)
